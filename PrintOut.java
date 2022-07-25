e332324c36de8efca648789638e2d3aa75bfdb4b
xlopxone <erwan.fadlys@gmail.com>
2022-07-25 19:47:29 +0300

    Fixed git

---
+++ PrintOut.java
@@ -0,0 +1,20 @@
+package JavaUdemy;
+// TASK 1: Make a class.
+public class PrintOut {
+    public static void main(String[] args) {
+        System.out.println("Printing is fun!");
+        System.out.println("Java > Pythin");
+        System.out.println("I spilled Java all over my paper");
+        System.out.println("My dog ate my Java");
+    }
+}
+
+
+
+// TASK 2: Inside the class, write the main() method.
+
+
+// TASK 3: Print these messages to the console: "Printing is fun!" 
+                                              // "Java > Python."
+                                              // "I spilled Java all over my paper."
+                                              // "My dog ate my Java." 
