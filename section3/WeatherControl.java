95949e369a9870d611778d76957d471dcfb10c72
xlopxone <erwan.fadlys@gmail.com>
2022-07-26 12:12:18 +0300

    daily commit

---
+++ section3/WeatherControl.java
@@ -0,0 +1,28 @@
+package section3;
+public class WeatherControl {
+        public static void main(String[] args) {
+    
+            int temp = 8;  
+    
+            String forecast = "";
+            
+            if (temp < -15 || temp < -1 ){
+                System.out.println("The forecast is FREEZING! Stay home!");
+            }else if (temp < 0 || temp < 10 ){
+                System.out.println("The forecast is Chilly. Wear a coat!");
+            }else{
+                System.out.println("else: It's warm. go outside!");
+            }
+            //IF - ELSE IF - ELSE STATEMENTS HERE!
+    
+            //if temp between -15 and -1: "The forecast is FREEZING! Stay home!"
+    
+            //if temp between 0 and 10: "The forecast is Chilly. Wear a coat!";
+    
+            //else: It's warm. go outside!
+    
+            System.out.println(forecast);
+        }
+    }
+    
+
