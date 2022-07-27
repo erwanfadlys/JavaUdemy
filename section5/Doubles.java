9f13baded3bb2ff594b500e156acdbe86145d8cb
xlopxone <erwan.fadlys@gmail.com>
2022-07-27 23:27:29 +0300

    deleted .class and two more challenges

--- section5/Doubles.java
+++ section5/Doubles.java
@@ -4,10 +4,24 @@ public class Doubles {
         /* Task 2
             1. Call the rollDice() function twice.
             2. Store the return values in dice1 and dice2.   
-
+        
         */
-        int dice1;
-        int dice2;
+        
+        int dice1 = (diceRoll());
+        int dice2 = (diceRoll());
+
+
+        while (dice1 != dice2) {
+            dice1 = (diceRoll());
+            dice2 = (diceRoll());
+            System.out.println("Dice 1: " + dice1);
+            System.out.println("Dice 2: " + dice2);
+            if (dice1 == dice2) {
+                System.out.println("You rolled doubles!\n");
+            }else{
+                System.out.println("Bummers, again!\n");
+            }
+        }
 
    //      System.out.println("Dice 1: " + dice1);
    //      System.out.println("Dice 2: " + dice2 + "\n");
@@ -21,7 +35,10 @@ public class Doubles {
         // Task 4: After they roll doubles, print: You rolled doubles!
     }
 
-
+    public static int diceRoll() {
+        double randomNumber = Math.random() * 6;
+        return ((int)randomNumber+1);
+    }
     /** Task 1 
      * Function name: rollDice – rolls a dice between 1 and 6
      * @return randomNumber (int)
